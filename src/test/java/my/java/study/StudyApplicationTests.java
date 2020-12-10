220c07cf1198b16e87ecbaed626ab32b913fc4f1
LIGUOLEI <86436551@qq.com>
2020-12-10 15:38:43 +0800

    初始化

---
+++ src/test/java/my/java/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package my.java.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
